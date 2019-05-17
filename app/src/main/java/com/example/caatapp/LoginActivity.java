c1b49299b1777b6dfd47630975941c12c84b7a1e
emresutuna <emre.sutuna1998@gmail.com>
2019-05-17 15:43:55 +0300

    modified

--- app/src/main/java/com/example/caatapp/LoginActivity.java
+++ app/src/main/java/com/example/caatapp/LoginActivity.java
@@ -1,13 +1,25 @@
 package com.example.caatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.androidquery.AQuery;
 
 public class LoginActivity extends AppCompatActivity {
-
+AQuery aQuery;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        aQuery=new AQuery(this);
+        aQuery.id(R.id.registerButton).getTextView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/caatapp/RegisterActivity.java
+++ app/src/main/java/com/example/caatapp/RegisterActivity.java
@@ -2,12 +2,29 @@ package com.example.caatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.androidquery.AQuery;
 
 public class RegisterActivity extends AppCompatActivity {
-
+AQuery aQuery;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        aQuery=new AQuery(this);
+    }
+    void valid(){
+        if(aQuery.id(R.id.usernameEtReg).getEditText().getText().length()<0){
+            Log.d("UsernameReg","Length Error");
+        }
+        if (aQuery.id(R.id.passwordEtReg).getEditText().getText().length()<5){
+            Log.d("Password Register","Length Error");
+        }else if(aQuery.id(R.id.passwordRegR).getEditText().getText().toString()!=aQuery.id(R.id.passwordEtReg).getEditText().getText().toString()){
+            Log.d("Repeat Resister Pass","Password not same");
+        }
+        else  if(aQuery.id(R.id.ageEt).getEditText().getText().length()<0){
+            Log.d("Age Register","Lenght Error");
+        }
     }
 }
