b9d719daad800b73f5e7a6313fe9016b9b1b0d50
emresutuna <emre.sutuna1998@gmail.com>
2019-05-17 19:28:14 +0300

    Login&Register Upcoming

--- app/src/main/java/com/example/caatapp/RegisterActivity.java
+++ app/src/main/java/com/example/caatapp/RegisterActivity.java
@@ -1,20 +1,76 @@
 package com.example.caatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.androidquery.AQuery;
 
 public class RegisterActivity extends AppCompatActivity {
 AQuery aQuery;
+public int sexStatus=-1;
+String email,password;
+RelativeLayout getForm,boyRel,girRel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         aQuery=new AQuery(this);
+        getForm=(RelativeLayout)findViewById(R.id.registerEmail );
+        boyRel=(RelativeLayout)findViewById(R.id.constraintLayout3 );
+        girRel=(RelativeLayout)findViewById(R.id.constraintLayout4 );
+        aQuery.id(R.id.girlButton).getImageView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sexStatus=0;
+                aQuery.id(R.id.buttonCont).getButton().setVisibility(View.VISIBLE);
+                aQuery.id(R.id.buttonCont).getButton().setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        clickContinue();
+                    }
+                });
+                Log.d("GirlButtonClicked", String.valueOf(sexStatus));
+            }
+        });
+        aQuery.id(R.id.boyButton).getImageView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sexStatus=1;
+                aQuery.id(R.id.buttonCont).getButton().setVisibility(View.VISIBLE);
+                aQuery.id(R.id.buttonCont).getButton().setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        clickContinue();
+                    }
+                });
+                Log.d("BoyButtonClicked", String.valueOf(sexStatus));
+            }
+        });
     }
-    void valid(){
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    void clickContinue(){
+        aQuery.id(R.id.boyButton).getImageView().setVisibility(View.GONE);
+        aQuery.id(R.id.girlButton).getImageView().setVisibility(View.GONE);
+        aQuery.id(R.id.textView2).getTextView().setVisibility(View.GONE);
+        aQuery.id(R.id.buttonCont).getButton().setVisibility(View.GONE);
+        boyRel.setVisibility(View.GONE);
+        girRel.setVisibility(View.GONE);
+        getForm.setVisibility(View.VISIBLE);
+
+
+    }
+
+    /* void valid(){
         if(aQuery.id(R.id.usernameEtReg).getEditText().getText().length()<0){
             Log.d("UsernameReg","Length Error");
         }
@@ -26,5 +82,5 @@ AQuery aQuery;
         else  if(aQuery.id(R.id.ageEt).getEditText().getText().length()<0){
             Log.d("Age Register","Lenght Error");
         }
-    }
+    }*/
 }
--- app/src/main/java/com/example/caatapp/SplashActivity.java
+++ app/src/main/java/com/example/caatapp/SplashActivity.java
@@ -3,26 +3,32 @@ package com.example.caatapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.androidquery.AQuery;
 
 public class SplashActivity extends AppCompatActivity {
-
+AQuery aQuery;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
-        Thread timerThread = new Thread(){
-            public void run(){
-                try{
-                    sleep(4000);
-                }catch(InterruptedException e){
-                    e.printStackTrace();
-                }finally{
-                    Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
-                    startActivity(intent);
-                }
-            }
-        };
-        timerThread.start();
+
+aQuery=new AQuery(this);
+aQuery.id(R.id.goLogin).getButton().setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+        startActivity(intent);
+    }
+});
+aQuery.id(R.id.goRegister).getButton().setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(SplashActivity.this,RegisterActivity.class);
+        startActivity(intent);
+    }
+});
     }
 }
